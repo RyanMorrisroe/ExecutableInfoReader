30b4dd74365b7f0c7a1bce92eafa3455c030924b
Ryan Morrisroe <6473229+RyanMorrisroe@users.noreply.github.com>
2019-10-28 01:00:03 -0500

    Well that's an awkward typo

--- ExecutableInfoReader/ExecutableInfoReader/ExecutableInfoReader/ExecutableInfoReader.ps1
+++ ExecutableInfoReader/ExecutableInfoReader/ExecutableInfoReader/ExecutableInfoReader.ps1
@@ -24,10 +24,10 @@ if($loadProductVersion) {
 
 	Write-Host("Product Version is " + $productVersion);
 	CreateVariable "$variableRoot.ProductVersionInfo.Version" $productVersion;
-	CreateVariable "$variableRoot.ProductVersionInfo.Verson.Major" $productVersion.Major;
-	CreateVariable "$variableRoot.ProductVersionInfo.Verson.Minor" $productVersion.Minor;
-	CreateVariable "$variableRoot.ProductVersionInfo.Verson.Build" $productVersion.Build;
-	CreateVariable "$variableRoot.ProductVersionInfo.Verson.Revision" $productVersion.Revision;
+	CreateVariable "$variableRoot.ProductVersionInfo.Version.Major" $productVersion.Major;
+	CreateVariable "$variableRoot.ProductVersionInfo.Version.Minor" $productVersion.Minor;
+	CreateVariable "$variableRoot.ProductVersionInfo.Version.Build" $productVersion.Build;
+	CreateVariable "$variableRoot.ProductVersionInfo.Version.Revision" $productVersion.Revision;
 }
 
 if($loadAssemblyVersion) {
@@ -36,18 +36,18 @@ if($loadAssemblyVersion) {
 
 		Write-Host("Assembly Version is " + $assemblyVersion);
 		CreateVariable "$variableRoot.AssemblyVersionInfo.Version" $assemblyVersion;
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Major" $assemblyVersion.Major;
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Minor" $assemblyVersion.Minor;
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Build" $assemblyVersion.Build;
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Revision" $assemblyVersion.Revision;
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Major" $assemblyVersion.Major;
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Minor" $assemblyVersion.Minor;
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Build" $assemblyVersion.Build;
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Revision" $assemblyVersion.Revision;
 	}
 	catch {
 		Write-Warning("Could not load assembly information - likely because the executable does not contain a manifest");
 		Write-Warning("Writing empty values for assembly variables");
 		CreateVariable "$variableRoot.AssemblyVersionInfo.Version" "";
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Major" "";
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Minor" "";
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Build" "";
-		CreateVariable "$variableRoot.AssemblyVersionInfo.Verson.Revision" "";
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Major" "";
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Minor" "";
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Build" "";
+		CreateVariable "$variableRoot.AssemblyVersionInfo.Version.Revision" "";
 	}
 }
